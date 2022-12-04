9a16731015ad2e6d9633c1f485ea6a961c322095
Christoffer Berg <christofferberg79@gmail.com>
2022-12-04 14:51:33 +0100

    Puzzle 4, part 2

--- aoc2022.exs
+++ aoc2022.exs
@@ -103,15 +103,30 @@ end
 
 defmodule Puzzle4 do
   def part1(input) do
+    parse(input)
+    |> Enum.count(&has_full_overlap/1)
+  end
+
+  def part2(input) do
+    parse(input)
+    |> Enum.count(&has_some_overlap/1)
+  end
+
+  defp parse(input) do
     input
     |> Enum.map(fn line ->
       String.split(line, ",")
       |> Enum.flat_map(&(String.split(&1, "-")))
       |> Enum.map(&String.to_integer/1)
     end)
-    |> Enum.count(fn [x1, x2, y1, y2] ->
-      x1 <= y1 and x2 >= y2 or y1 <= x1 and y2 >= x2
-    end)
+  end
+
+  defp has_full_overlap([x1, x2, y1, y2]) do
+    x1 <= y1 and x2 >= y2 or y1 <= x1 and y2 >= x2
+  end
+
+  defp has_some_overlap([x1, x2, y1, y2]) do
+    x1 <= y2 and x2 >= y1
   end
 end
 
@@ -135,6 +150,8 @@ defmodule AoC2022 do
     run("input/day3.txt", &Puzzle3.part2/1)
     # 466
     run("input/day4.txt", &Puzzle4.part1/1)
+    # 865
+    run("input/day4.txt", &Puzzle4.part2/1)
   end
 end
 
